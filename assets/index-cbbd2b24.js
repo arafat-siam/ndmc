f4ceee396819d46139ff4f3c6715542842f0bb5b
arafat-siam <mdarafatsiam1590@gmail.com>
2023-11-11 00:23:04 +0600

    Update

--- assets/index-cbbd2b24.js
+++ assets/index-cbbd2b24.js
@@ -235,8 +235,7 @@ background-color: transparent;
 background: ${$1().palette.mode==="dark"?"rgba(255, 255, 255, 0.1)":"rgba(255, 255, 255, 0.3)"};
   border-radius:10px;
   box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-  backdrop-filter: blur(15.5px);
-  --webkit-backdrop-filter: blur(15.5px);
+ 
 
 
 
